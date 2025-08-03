ddb833173138e6e97510f1bd8e4268206ec404cc
MinkieYume <sign@yumieko.com>
2025-08-03 21:03:11 +0800

    3

--- neko/src/main.c
+++ neko/src/main.c
@@ -130,7 +130,7 @@ int main(int argc, char **argv) {
     sexp ctx = init_sexp();
     nng_socket *sock = NULL;
     nng_socket node;
-    if (argc >= 2) {
+    if (argc >= 3) {
 	node = init_node(argc, argv);
 	sock = &node;
 	if (sock != NULL) {
